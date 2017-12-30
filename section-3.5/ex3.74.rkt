d62e852ea98eb231bc33136ebb6f09fc56659ac9
Chaitanya Gupta <mail@chaitanyagupta.com>
2017-12-31 03:13:53 +0530

    Meta circular evaluator till 4.9

---
+++ section-3.5/ex3.74.rkt
@@ -0,0 +1,66 @@
+#lang sicp
+
+(define (stream-car stream) (car stream))
+(define (stream-cdr stream) (force (cdr stream)))
+
+(define (stream-ref s n)
+  (if (= n 0)
+      (stream-car s)
+      (stream-ref (stream-cdr s) (- n 1))))
+
+(define (stream-map proc . argstreams)
+  (if (stream-null? (car argstreams))
+      the-empty-stream
+      (cons-stream
+       (apply proc (map stream-car argstreams))
+       (apply stream-map
+              (cons proc (map stream-cdr argstreams))))))
+
+(define (stream-filter pred stream)
+  (cond ((stream-null? stream) the-empty-stream)
+        ((pred (stream-car stream))
+         (cons-stream (stream-car stream)
+                      (stream-filter pred
+                                     (stream-cdr stream))))
+        (else (stream-filter pred (stream-cdr stream)))))
+
+(define (stream-for-each proc s)
+  (if (stream-null? s)
+      'done
+      (begin (proc (stream-car s))
+             (stream-for-each proc (stream-cdr s)))))
+
+(define (display-stream s limit)
+  (display #\()
+  (define (iter stream n)
+    (if (and (not (null? stream))
+             (> n 0))
+        (begin (if (< n limit) (display #\ ))
+               (display (stream-car stream))
+               (iter (stream-cdr stream) (- n 1)))))
+  (iter s limit)
+  (display #\)))
+
+(define (sign-change-detector new-value last-value)
+  (cond ((and (< new-value 0) (> last-value 0)) -1)
+        ((and (> new-value 0) (< last-value 0)) +1)
+        (else 0)))
+
+(define (make-zero-crossings input-stream last-value)
+  (if (stream-null? input-stream)
+      the-empty-stream
+      (cons-stream
+       (sign-change-detector (stream-car input-stream) last-value)
+       (make-zero-crossings (stream-cdr input-stream)
+                            (stream-car input-stream)))))
+
+(define (list-stream list)
+  (if (null? list)
+      the-empty-stream
+      (cons-stream (car list)
+                   (list-stream (cdr list)))))
+
+(define sense-data (list-stream '(1  2  1.5  1  0.5  -0.1  -2  -3  -2  -0.5  0.2  3  4)))
+
+(define zero-crossings
+  (stream-map sign-change-detector sense-data (cons-stream 0 sense-data)))
---
+++ section-3.5/ex3.75.rkt
@@ -0,0 +1,68 @@
+#lang sicp
+
+(define (stream-car stream) (car stream))
+(define (stream-cdr stream) (force (cdr stream)))
+
+(define (stream-ref s n)
+  (if (= n 0)
+      (stream-car s)
+      (stream-ref (stream-cdr s) (- n 1))))
+
+(define (stream-map proc . argstreams)
+  (if (stream-null? (car argstreams))
+      the-empty-stream
+      (cons-stream
+       (apply proc (map stream-car argstreams))
+       (apply stream-map
+              (cons proc (map stream-cdr argstreams))))))
+
+(define (stream-filter pred stream)
+  (cond ((stream-null? stream) the-empty-stream)
+        ((pred (stream-car stream))
+         (cons-stream (stream-car stream)
+                      (stream-filter pred
+                                     (stream-cdr stream))))
+        (else (stream-filter pred (stream-cdr stream)))))
+
+(define (stream-for-each proc s)
+  (if (stream-null? s)
+      'done
+      (begin (proc (stream-car s))
+             (stream-for-each proc (stream-cdr s)))))
+
+(define (display-stream s limit)
+  (display #\()
+  (define (iter stream n)
+    (if (and (not (null? stream))
+             (> n 0))
+        (begin (if (< n limit) (display #\ ))
+               (display (stream-car stream))
+               (iter (stream-cdr stream) (- n 1)))))
+  (iter s limit)
+  (display #\)))
+
+(define (sign-change-detector new-value last-value)
+  (cond ((and (< new-value 0) (> last-value 0)) -1)
+        ((and (> new-value 0) (< last-value 0)) +1)
+        (else 0)))
+
+(define (make-zero-crossings input-stream last-value last-avpt)
+  (if (stream-null? input-stream)
+      the-empty-stream
+      (let ((avpt (/ (+ (stream-car input-stream) last-value) 2)))
+        (cons-stream (sign-change-detector avpt last-avpt)
+                     (make-zero-crossings (stream-cdr input-stream)
+                                          (stream-car input-stream)
+                                          avpt)))))
+
+(define (list-stream list)
+  (if (null? list)
+      the-empty-stream
+      (cons-stream (car list)
+                   (list-stream (cdr list)))))
+
+(define sense-data (list-stream '(1  2  1.5  1  0.5  -0.1  -2  -3  -2  -0.5  0.2  3  4)))
+(define zero-crossings (make-zero-crossings sense-data 0 0))
+
+(define sense-data-2 (list-stream '(1  2  1.5  1  0.5  -0.1  -2  1  -2  -0.5  0.2  3  4)))
+(define zero-crossings-2 (make-zero-crossings sense-data-2 0 0))
